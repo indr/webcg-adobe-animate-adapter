7fe4d7bc88e08c393fd59244e70b458c0dfff414
indr <mail@indr.ch>
2018-08-30 16:29:25 +0800

    chore: add explicit dependency on webcg-devtools

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -2,5 +2,5 @@
 
 mkdir -p dist
 rm -rf dist/*
-cp node_modules/webcg-framework/dist/webcg-devtools.umd.js dist/
+cp node_modules/webcg-devtools/dist/webcg-devtools.umd.js dist/
 cp adobe-animate/* dist/
